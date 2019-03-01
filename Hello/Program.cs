fe4ac25cf78c37332056e4412698f3a2737a5a7e
frank <154902823@qq.com>
2019-03-01 16:59:20 +0800

    resetcolor

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,20 +10,14 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            if (args.Length >= 2) { 
-              System.Console.WriteLine("Hello World!{0} is {1}", args[0], args[1]);
-            }
-            else
-            {
-                System.Console.WriteLine("Hello World! command line parameter does not meet the requirement!");
-            }
             Console.BackgroundColor = ConsoleColor.Green;
             System.Console.WriteLine("Hello World!");
             System.Console.WriteLine("Hello World!");
             Console.ForegroundColor = ConsoleColor.Yellow;
             System.Console.WriteLine("Hello World!");
             Console.ResetColor();
-            
+
         }
+       
     }
 }
